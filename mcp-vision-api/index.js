da0c2d3c46673dc162b3f8b0752dc4a24591d3ad
LucasNavarroB <lucas.navarro@sansano.usm.cl>
2024-02-29 23:02:33 -0300

    Update index.js

--- mcp-vision-api/index.js
+++ mcp-vision-api/index.js
@@ -8,10 +8,15 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-
 app.use(cors({
   origin: '*'
 }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // Permitir solicitudes desde cualquier origen
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // Permitir los métodos de solicitud que desees
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(helmet());
 app.use(bodyParser.urlencoded({extended:false,limit:"5mb"}));
 app.use(bodyParser.json({limit:"5mb"}));
