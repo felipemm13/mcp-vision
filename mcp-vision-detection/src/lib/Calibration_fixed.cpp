595fe01be664a27f0e1cd6f7e74e67c540cab05d
Ignacio Barrera <114948480+ignacio-barrera@users.noreply.github.com>
2024-03-22 01:26:19 -0300

    Deleted comments

--- mcp-vision-detection/src/lib/Calibration_fixed.cpp
+++ mcp-vision-detection/src/lib/Calibration_fixed.cpp
@@ -1625,7 +1625,7 @@ std::tuple<int,std::string ,std::vector<PointWithContour>, std::string, int, int
     for (int y = 0; y < ext_global_mask_filtered.rows; ++y) {
         for (int x = 0; x < ext_global_mask_filtered.cols; ++x) {
             if (ext_global_mask_filtered.at<uchar>(y, x) == 0) { 
-                if (!compareColorsAt(M1m, x, y, color_ref, 80)) {
+                if (!compareColorsAt(M1m, x, y, color_ref, 60)) {
                     ext_global_mask_filtered.at<uchar>(y, x) = 255;
     }}}}
     //===============================================================================================//
@@ -2024,7 +2024,7 @@ std::tuple<int,std::string ,std::vector<PointWithContour>, std::string, int, int
     for (int y = 0; y < ext_global_mask_filtered.rows; ++y) {
         for (int x = 0; x < ext_global_mask_filtered.cols; ++x) {
             if (ext_global_mask_filtered.at<uchar>(y, x) == 0) { 
-                if (!compareColorsAt(M1m, x, y, color_ref, 80)) {
+                if (!compareColorsAt(M1m, x, y, color_ref, 60)) {
                     ext_global_mask_filtered.at<uchar>(y, x) = 255;
     }}}}
     //===============================================================================================//
